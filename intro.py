6c97fdfcc1720d7a27ae737f025760c19cbbc6d1
Sagar <sagar@example.com>
2026-05-19 16:20:45 +0530

    version 1

---
+++ intro.py
@@ -0,0 +1,52 @@
+
+# INTRO TO PYTHON
+
+
+# Python is a beginner-friendly programming language.
+# It reads almost like the English language.
+# No need for semicolons or curly braces like other languages.
+# Python mainly uses indentation to define blocks of code.
+
+# --- Your First Program
+print("Hello, World!")   # This prints text to the screen
+
+
+# How to add comments....
+
+# This is a single-line comment (Python ignores this)
+
+"""
+This is a
+multi-line comment
+hjiugo
+"""
+
+
+#Taking input from user 
+
+name = input("What is your name? ")
+# asks the user to type something
+
+print("Hello,", name)   # greets the user back
+
+# By default, input() stores data as a string.
+
+
+#  print() tricks
+
+print("I", "love", "India")           # I love India
+print("I", "love", "India", sep="-") # I-love-India
+
+print("Hello", end=" ")              # no newline at end
+print("World")                       # Hello World
+
+
+#Python is case-sensitive
+
+Name = "SAGAR"
+name = "sagar"
+
+print(Name)   # SAGAR
+print(name)   # sagar
+
+# These are two different variables.
